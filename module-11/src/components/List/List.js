14038e47bc2cbebc1261dc81e45c38bca5ba51d2
Thomas Weber <tweber.1995@hotmail.com>
2024-03-22 19:42:48 +0100

    html loaded

---
+++ module-11/src/components/List/List.js
@@ -0,0 +1,25 @@
+const List = () => {
+    return (
+        <div>
+            <header>
+                <h1>Things to do <span>soon</span></h1>
+            </header>
+            <div>
+                <p>Interesting things I want to check out!</p>
+            </div>
+            <section>
+                <article>
+                   <h3>Books</h3> 
+                </article>
+                <article>
+                   <h3>Movies</h3> 
+                </article>
+                <article>
+                   <h3>Games</h3> 
+                </article>
+            </section>
+        </div>
+    )
+}
+
+export default List
