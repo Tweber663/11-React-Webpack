48f3258db40cf7ed2c017c221f70c423dcf801f3
Thomas Weber <tweber.1995@hotmail.com>
2024-04-03 16:40:24 +0200

    cards loaded correclty from warehouse redux

--- module-11/src/components/List/List.js
+++ module-11/src/components/List/List.js
@@ -1,75 +1,30 @@
 import styles from './List.module.scss'
 import Column from '../column/column'
 import ColumnForm from '../ColumnForm/ColumnForm';
+import { useSelector } from 'react-redux';
 
 //React hooks
-import shortid from 'shortid';
-import { useState } from 'react';
 
 const List = () => {
   
-
-    const [columns, setColumns] = useState([
-        {
-            id: 1,
-            title: 'Books',
-            icon: 'book',
-            cards: [
-                { id: 1, title: 'This is Going to Hurt' },
-                { id: 2, title: 'Interpreter of Maladies' }
-            ]
-        },
-        {
-            id: 2,
-            title: 'Movies',
-            icon: 'film',
-            cards: [
-                { id: 1, title: 'Harry Potter' },
-                { id: 2, title: 'Star Wars' }
-            ]
-        },
-        {
-            id: 3,
-            title: 'Games',
-            icon: 'gamepad',
-            cards: [
-                { id: 1, title: 'The Witcher' },
-                { id: 2, title: 'Skyrim' }
-            ]
-        }
-    ]);
-
-    const addColumn = element => {
-		setColumns([...columns, { id: shortid(), title: element.title, icon: element.icon, cards: [] }]);
-        console.log(element);
-    };
-
-    const addCard = (newCard, columnId) => {
-        const columnsUpdated = columns.map(column => {
-            if(column.id === columnId)
-                return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
-            else
-                return column
-        })
-    
-        setColumns(columnsUpdated);
-    
-    };
+    const columns = useSelector(state => state.columns);
 
     return (
         <div className={styles.list}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>Things to do <span>soon</span></h1>
-            </header>
-            <div>
-                <p className={styles.description}>Interesting things I want to check out!</p>
-            </div>
-            <section className={styles.columns}>
-                {columns.map((c) => <Column action={addCard} key={c.id} id={c.id} title={c.title} icon={c.icon} cards={c.cards}/>)}
-                <ColumnForm action={addColumn} />
-            </section>
+          <header className={styles.header}>
+            <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+          </header>
+          <p className={styles.description}>Interesting things I want to check out</p>
+          <section className={styles.columns}>
+            {columns.map(column =>
+              <Column
+                key={column.id}
+                {...column}  />
+            )}
+          </section>
+          <ColumnForm />
         </div>
-    )
+      );
 }
 
 export default List
--- module-11/src/components/column/column.js
+++ module-11/src/components/column/column.js
@@ -1,8 +1,13 @@
 import Card from '../Card/Card';
 import styles from './column.module.scss'
 import CardForm from '../cardForm/cardForm';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 const Column = (element) => {
+
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === element.id));
+
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}>
@@ -10,7 +15,7 @@ const Column = (element) => {
                 <span className={`${styles.icon} fa fa-${element.icon}`}></span>
             </h2>
             <ul className={styles.cards}>
-                {element.cards.map(card => <Card cards={card}/>)}
+                {cards.map(card => <Card cards={card}/>)}
                 <CardForm columnId={element.id} action={element.action}/>
             </ul>
 
