9eb444c8eb47d667c9c347c2d1ccec3b55ae2d80
Thomas Weber <tweber.1995@hotmail.com>
2024-04-10 13:16:23 +0200

    changes

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    </React.StrictMode>
+);
+reportWebVitals();
